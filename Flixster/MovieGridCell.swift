f3111d16b426d5ca653c86197bf1f9f8748cd3c4
Cyrus Simons <cyrus.simons@yahoo.com>
2020-09-16 14:38:40 -0400

    Finished CollectionView for superhero movies

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Cyrus Simons on 9/16/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
